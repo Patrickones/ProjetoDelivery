37297419c41b6cbc91b25a8011f31eda23a98c64
Patrick <patrickonesradaelli@gmail.com>
2022-12-05 19:36:49 -0300

    comitar

--- app/src/main/java/com/example/projetodelivery/MainActivity.java
+++ app/src/main/java/com/example/projetodelivery/MainActivity.java
@@ -2,18 +2,31 @@ package com.example.projetodelivery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_login = findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
 
 
     }
