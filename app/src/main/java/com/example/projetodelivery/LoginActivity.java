8ef08ff216bbd751ea58d8cb8bb28062a0fcf333
Patrickones <patrickonesradaelli@gmail.com>
2022-12-09 20:17:03 -0300

    aaa

--- app/src/main/java/com/example/projetodelivery/LoginActivity.java
+++ app/src/main/java/com/example/projetodelivery/LoginActivity.java
@@ -11,72 +11,33 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.Task;
 
 public class LoginActivity extends AppCompatActivity {
 
-    public static int GOOGLE_SIGN_IN = 1;
+    private EditText id_email;
+    private EditText id_senha;
+    private Button btn_entrar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-
-                .requestEmail()
-                .requestIdToken(getResources().getString(R.string.google_cliente_id))
-                .build();
-
-        // Build a GoogleSignInClient with the options specified by gso.
-        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-
-        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+        btn_entrar = (Button) findViewById(R.id.btn_entrar);
+        btn_entrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-                startActivityForResult(signInIntent,GOOGLE_SIGN_IN);
+                id_email = (EditText) findViewById(R.id.id_email);
+                id_senha = (EditText) findViewById(R.id.id_senha);
+                 if (id_email.getText().toString().equals("email") && id_senha.getText().toString().equals("1234")){
+                     setContentView(R.layout.activity_main);
+                     getSupportActionBar().show();
+                 }
             }
         });
+
+
     }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == GOOGLE_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
-        }
-    }
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-
-            finish();
-
-        } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Toast.makeText(this,"Falha ao autentica " + e.getStatusCode(),Toast.LENGTH_LONG).show();
-        }
-    }
-
-
-
-
-
-
 
 }
--- app/src/main/java/com/example/projetodelivery/MainActivity.java
+++ app/src/main/java/com/example/projetodelivery/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.projetodelivery;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainerView;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,21 +16,56 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn_lanches;
+    private Button btn_promocoes;
+    private Button btn_bebidas;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        if(account == null){
-            Intent intent = new Intent(MainActivity.this,LoginActivity.class);
-            startActivity(intent);
 
-        }
+
+        getSupportActionBar().hide();
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffffff")));
+
+        btn_promocoes = (Button) findViewById(R.id.btnPromocoes);
+        btn_promocoes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().remove(getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView)).commit();
+                getSupportFragmentManager().beginTransaction().setReorderingAllowed(true).add(R.id.fragmentContainerView, PromocoesFragment.class,null).commit();
+            }
+        });
+
+        btn_lanches = (Button) findViewById(R.id.btnLanches);
+        btn_lanches.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().remove(getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView)).commit();
+                getSupportFragmentManager().beginTransaction().setReorderingAllowed(true).add(R.id.fragmentContainerView, LanchesFragment.class,null).commit();
+            }
+        });
+
+        btn_bebidas = (Button) findViewById(R.id.btnBebidas);
+        btn_bebidas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().remove(getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView)).commit();
+                getSupportFragmentManager().beginTransaction().setReorderingAllowed(true).add(R.id.fragmentContainerView, BebidasFragment.class,null).commit();
+            }
+        });
 
 
 
     }
+
 
 
 
